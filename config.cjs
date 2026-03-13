ca9d414fffbc72bae8cd9884badd4eb382f33613
Christopher Smith <christopher.smith@oopscope.com>
2026-03-13 15:11:51 -0400

    SJS-V-0.1.1  - GasClient: Added logging for requests and errors, and improved error handling in interceptors.  - Updated tests and configuration files accordingly.

--- config.cjs
+++ config.cjs
@@ -1,21 +1,36 @@
 
+const common = {
+  logLevel: 'INFO', // Default log level
+};
+
 module.exports = {
-  dev: {
+  development: {
+    ...common,
+    platform: {
+      name: 'gas',
+      deploymentId: 'AKfycbxjR-rxzS5DbyIekcTbGbA6WinDmjBlSMEfWPjxE6ObbQLVNvyNx86fwMX2pu4BVmmL',
+    },
     version: 'SNAPSHOT-0.1.0',
-    deploymentId: 'AKfycbxjR-rxzS5DbyIekcTbGbA6WinDmjBlSMEfWPjxE6ObbQLVNvyNx86fwMX2pu4BVmmL',
   },
-  prod: {
+  production: {
+    ...common,
     // Production configuration
     // This is the configuration used in the live environment
     // It should be updated with the latest stable version and deployment ID
     // Ensure to update this when deploying new changes
-    // Example:
-    // deploymentId: 'AKfycbywslFpBHt1OcsTyaE_gCRFrd3wjGhaOtbwr7mpO-hTMyyurJBM2tAiKX8cksRfmySR',
-    // Note: The deploymentId should be the one from the latest deployment
-    // This is a placeholder and should be replaced
-    // with the actual deployment ID after deployment
+    platform: {
+      name: 'gas',
+      deploymentId: 'AKfycbyBxinKd-rRq3W5ggM2iH_FGe9seacTgJqjQ3N3LpGd-pIvt7hoFz8RBEjgkNAGnLUI',
+    },
     version: 'RELEASE-1.2.10',
-    deploymentId: 'AKfycbyBxinKd-rRq3W5ggM2iH_FGe9seacTgJqjQ3N3LpGd-pIvt7hoFz8RBEjgkNAGnLUI',
+  },
+  test: {
+    ...common,
+    platform: {
+      name: 'node',
+    },
+    version: 'TEST-0.0.0',
+    logLevel: 'DEBUG', // Override for tests
   }
 
 };
